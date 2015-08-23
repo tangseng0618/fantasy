7c6e7f22c572fbd4dc2efae42a76d73df8060279
it0618 <1306386607@qq.com>
2015-08-23 19:06:51 +0800

    fantasy项目代码初始化

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bench/fantasy/admin/');
+define('HTTP_CATALOG', 'http://localhost/bench/fantasy/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bench/fantasy/admin/');
+define('HTTPS_CATALOG', 'http://localhost/bench/fantasy/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/abel/Sites/bench/fantasy/admin/');
+define('DIR_SYSTEM', '/Users/abel/Sites/bench/fantasy/system/');
+define('DIR_LANGUAGE', '/Users/abel/Sites/bench/fantasy/admin/language/');
+define('DIR_TEMPLATE', '/Users/abel/Sites/bench/fantasy/admin/view/template/');
+define('DIR_CONFIG', '/Users/abel/Sites/bench/fantasy/system/config/');
+define('DIR_IMAGE', '/Users/abel/Sites/bench/fantasy/image/');
+define('DIR_CACHE', '/Users/abel/Sites/bench/fantasy/system/cache/');
+define('DIR_DOWNLOAD', '/Users/abel/Sites/bench/fantasy/system/download/');
+define('DIR_UPLOAD', '/Users/abel/Sites/bench/fantasy/system/upload/');
+define('DIR_LOGS', '/Users/abel/Sites/bench/fantasy/system/logs/');
+define('DIR_MODIFICATION', '/Users/abel/Sites/bench/fantasy/system/modification/');
+define('DIR_CATALOG', '/Users/abel/Sites/bench/fantasy/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'fantasy');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bench/fantasy/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bench/fantasy/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/abel/Sites/bench/fantasy/catalog/');
+define('DIR_SYSTEM', '/Users/abel/Sites/bench/fantasy/system/');
+define('DIR_LANGUAGE', '/Users/abel/Sites/bench/fantasy/catalog/language/');
+define('DIR_TEMPLATE', '/Users/abel/Sites/bench/fantasy/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/abel/Sites/bench/fantasy/system/config/');
+define('DIR_IMAGE', '/Users/abel/Sites/bench/fantasy/image/');
+define('DIR_CACHE', '/Users/abel/Sites/bench/fantasy/system/cache/');
+define('DIR_DOWNLOAD', '/Users/abel/Sites/bench/fantasy/system/download/');
+define('DIR_UPLOAD', '/Users/abel/Sites/bench/fantasy/system/upload/');
+define('DIR_MODIFICATION', '/Users/abel/Sites/bench/fantasy/system/modification/');
+define('DIR_LOGS', '/Users/abel/Sites/bench/fantasy/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'fantasy');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
